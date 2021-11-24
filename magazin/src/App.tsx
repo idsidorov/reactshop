a46884f956ab5772d2642f5f56a232d28c913e33
danil-react <da.kuharenko@justiceteam-it.com>
2021-11-24 14:18:57 +0300

    тест

--- magazin/src/App.tsx
+++ magazin/src/App.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import NavBar from './catalog/components/NavBar/NavBar';
-import PageCatalog from './catalog/components/PageCatalog/PageCatalog';
-
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 const App: React.FC = () => {
   return (
-    <NavBar />
+    <BrowserRouter>
+          <Navbar />
+          <div className="container">
+             <Switch>
+                 <Route component={PageTodoList} path="/todo" />
+                 <Route component={PageAboutUs}  path="/about" />
+                 <Route component={PageSignIn}  path="/signIn" />
+                 <Route component={PageSignUp}  path="/signUp" />
+             </Switch>
+          </div>
+      </BrowserRouter>
+  
   );
 }
 
--- magazin/src/catalog/components/NavBar/NavBar.tsx
+++ magazin/src/catalog/components/NavBar/NavBar.tsx
@@ -1,26 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
-const NavBar: React.FC = () => (
-    <nav>
-        <div className='nav-wrapper'>
-            <a href="#!" className="brand-logo"><i className="material-icons">cloud</i>InterShop</a>
-            <ul className="right hide-on-med-and-down">
-                <li>
-                    <form>
-                        <div className="input-field">
-                            <input id="search" type="search" required />
-                            <label className="label-icon" htmlFor="search">
-                            <i className="material-icons">search</i></label>
-                            <i className="material-icons">close</i>
-                        </div>
-                    </form>
-                </li>
-                <li><a href="/"><i className="material-icons">account_circle</i></a></li>
-                <li><a href="/"><i className="material-icons">shopping_cart</i></a></li>
-                <li><a href="/"><i className="material-icons">history</i></a></li>
-            </ul>
-        </div>
-    </nav>
-)
+const NavBar: React.FC = () => {
+
+    const [isLogin, setIsLogin] = useState<boolean> (false)
+
+
+
+    return (
+        <nav>
+            <div className='nav-wrapper'>
+                <a href="#!" className="brand-logo"><i className="material-icons">cloud</i>InterShop</a>
+                <ul className="right hide-on-med-and-down">
+                    <li>
+                        <form>
+                            <div className="input-field">
+                                <input id="search" type="search" required />
+                                <label className="label-icon" htmlFor="search">
+                                    <i className="material-icons">search</i></label>
+                                <i className="material-icons">close</i>
+                            </div>
+                        </form>
+                    </li>
+                        {isLogin ? <button className="btn waves-effect waves-light" type="submit" name="action">Купить</button> : <li><a href="/"><i className="material-icons">account_circle</i></a></li>}
+                        <a href="/"><i className="material-icons">account_circle</i></a>
+                    <li><a href="/"><i className="material-icons">shopping_cart</i></a></li>
+                    <li><a href="/"><i className="material-icons">history</i></a></li>
+                </ul>
+            </div>
+        </nav>
+    )
+}
+
+    
+
 
 export default NavBar;
